4c7446c8e59bf66b0485f20026cd7ca41ff5b612
FerjSuarez <ferjsuarez@gmail.com>
2025-07-30 19:38:12 -0300

    Desafio1_Respuesta

--- Clase 11/desafio1.py
+++ Clase 11/desafio1.py
@@ -8,12 +8,27 @@ def calcular_area_triangulo(base, altura):
 def calcular_area_rectangulo(base, altura):
     return base * altura
 
-base_triangulo = 5
-altura_triangulo = 8
-area_triangulo = calcular_area_triangulo(base_triangulo, altura_triangulo)
-print(f"Área del triángulo: {area_triangulo}")
+class Triangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
 
-base_rectangulo = 6
-altura_rectangulo = 4
-area_rectangulo = calcular_area_rectangulo(base_rectangulo, altura_rectangulo)
-print(f"Área del rectángulo: {area_rectangulo}")
+    def calcular_area(self):
+        return (self.base * self.altura) / 2
+    
+class Rectangulo:
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+triangulo = Triangulo(5, 8)
+calcular_area_triangulo = triangulo.calcular_area()
+print(f"Área del triángulo: {calcular_area_triangulo}")
+
+rectangulo = Rectangulo(6, 4)
+calcular_area_rectangulo = rectangulo.calcular_area()
+print(f"Área del rectángulo: {calcular_area_rectangulo}")
+
